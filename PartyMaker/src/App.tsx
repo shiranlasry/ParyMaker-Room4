12dd0e943d3c2bda218d574af7ac3fd8fcdc7855
Doriel Shacham <dorielshacham@gmail.com>
2023-12-25 17:58:17 +0200

    test users for now

--- PartyMaker/src/App.tsx
+++ PartyMaker/src/App.tsx
@@ -1,22 +1,21 @@
-//App.tsx is the root component of the app.
-import './App.scss'
-import Home from './pages/home/Home'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Page404 from './pages/page404/Page404'
-import LogIn from './pages/logIn/LogIn'
-import Register from './pages/register/Register'
+import './App.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Page404 from './pages/page404/Page404';
+import LogIn from './pages/logIn/LogIn';
+import Register from './pages/register/Register';
 
 function App() {
-  const router= createBrowserRouter([
-    { path: "/", element: <Home /> },
-    { path: "/login", element: <LogIn />},
-    { path: "/register", element: <Register />},
-    { path: "*", element: <Page404 /> },
-  ])
-
   return (
-    <RouterProvider router={router} />
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
