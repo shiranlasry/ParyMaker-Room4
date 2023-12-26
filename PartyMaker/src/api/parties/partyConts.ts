c1e29349f2bbc85a0c53330ef39971842d03c1bc
hadaritzhaki <127751975+hadaritzhaki@users.noreply.github.com>
2023-12-26 17:01:54 +0200

    crud (party)

---
+++ PartyMaker/src/api/parties/partyConts.ts
@@ -0,0 +1,76 @@
+
+import {Party} from "./partyModel";
+let parties: Party[] = [];
+
+export const getParties = (req,res) =>{
+    try {
+        res.send({parties})
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+export const addParty = (req, res) => {
+    try {
+        const party: Party = req.body;
+        const createParty = (data: Party): Party => ({
+            partyName: data.partyName,
+            partyDate: data.partyDate,
+            partyTime: data.partyTime,
+            partyLocation: data.partyLocation,
+            partyType: data.partyType,
+            partyDescription: data.partyDescription,
+            partyPrice: data.partyPrice,
+            partyImage: data.partyImage,
+            partyCreator: data.partyCreator,
+            partyParticipants:data.partyParticipants,
+            createdAt: data.createdAt,
+            id:data.id,
+        });
+        parties.push(createParty(party));
+
+        res.send({ parties });
+    } catch (error) {
+        console.error(error.message);
+    }
+};
+
+export const deleteParty = (req, res) =>{
+    try {
+        const {id} = req.body;
+        parties= parties.filter((party) =>party.id !== id);
+        res.send({parties})
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+export const updateParty= (req, res) =>{
+    try {
+        const {partyName,partyDate,partyTime,partyLocation, partyType,partyDescription, partyPrice,partyImage,partyCreator,partyParticipants,createdAt,id} = req.body;
+        console.log(req.body);
+        if (!partyName || !id ||!partyDate || !partyTime || !partyLocation || !partyType || !partyDescription || !partyPrice ||!partyImage || !partyCreator || !partyParticipants || !createdAt) throw new Error("Please complete all fields");
+        const party = parties.find((party) => party.id === id);
+
+        if (!party) throw new Error("party not found");
+        party.partyName = partyName;
+        party.partyDate = partyDate;
+        party.partyTime = partyTime;
+        party.partyLocation = partyLocation;
+        party.partyType = partyType;
+        party.partyDescription = partyDescription;
+        party.partyPrice = partyPrice;
+        party.partyImage = partyImage;
+        party.partyCreator = partyCreator;
+        party.partyParticipants = partyParticipants;
+        party.createdAt = createdAt;
+        party.id = id;
+
+        res.send({party});
+    } catch (error) {
+        console.error(error);
+        res.send({ error });
+    }
+    
+
+}
