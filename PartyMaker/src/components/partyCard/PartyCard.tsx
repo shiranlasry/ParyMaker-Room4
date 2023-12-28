a86cb8f429708e8fed181489d74823fc57f1cdab
shiranlasry <128288093+shiranlasry@users.noreply.github.com>
2023-12-28 20:45:41 +0200

    finish

--- PartyMaker/src/components/partyCard/PartyCard.tsx
+++ PartyMaker/src/components/partyCard/PartyCard.tsx
@@ -1,6 +1,7 @@
 // PartyCard.jsx
 import React from 'react';
 import { Party } from '../../types-env';
+import './partyCard.scss';
 
 type PartyCardProps = {
     party: Party;
@@ -8,9 +9,19 @@ type PartyCardProps = {
   
   const PartyCard: React.FC<PartyCardProps> = ({ party }) => {
     return (
-      <div className="party-card">
-        <h2>{party.partyName}</h2>
-        {/* Add more details or styling as needed */}
+        <div className="party-card">
+        <img src={party.partyImage} alt={party.partyName} className="party-image" />
+        <div className="party-details">
+          <h2>{party.partyName}</h2>
+          <p>Date: {party.partyDate}</p>
+          <p>Time: {party.partyTime}</p>
+          <p>Location: {party.partyLocation}</p>
+          <p>Type: {party.partyType.partyTypeName}</p>
+          <p>Creator: {party.partyCreator.firstName} {party.partyCreator.lastName}</p>
+          <p>Participants: {party.partyParticipants.length}</p>
+          <p>Things to bring: {party.thingsToBring?.join(', ')}</p>
+        </div>
+        <button className="join-button">Join Party</button>
       </div>
     );
   };
