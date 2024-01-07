e108111aaeb5b39f1a9586c509ac34605be39b5c
DorielShacham <dorielshacham@gmail.com>
2024-01-07 17:58:13 +0200

    Hero container updated

---
+++ PartyMaker/src/components/hero/Hero.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './hero.css';
+
+const Hero = () => {
+  return (
+    <>
+        <div className="container-fluid hero-container">
+            <div className="row">
+                <div className="wrappers">
+                    <h1 className="font-weight-bold">Welcome to PartyPlanner. Plan your party at a click of a button.</h1>
+                    <p>Whatever your interest, from hiking and reading to networking and skill sharing, there are thousands of people who share it on PartyPlanner. Events are happening every day—sign up to join the fun.</p>
+                </div>
+                    <div className="wrappers">
+                        <img src="https://wallpapercave.com/wp/wp7713640.jpg" className='image-container rounded-top-right' alt="PartyPlanner" />
+                    </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Hero
--- PartyMaker/src/pages/home/Home.tsx
+++ PartyMaker/src/pages/home/Home.tsx
@@ -3,13 +3,14 @@ import './home.scss'
 import NavBar from '../../components/navBar/NavBar'
 import { useNavigate } from 'react-router';
 import HOtParties from '../../components/hotParties/HOtParties';
+import Hero from '../../components/hero/Hero';
 
 const Home = () => {
     const navigate = useNavigate();
   return (
     <div>
         <NavBar />
-    
+        <Hero />
 
        <h2 className='upcoming'>Upcoming Events 📅</h2>
 
