0471e7e16e704e4664eef2095fd5d4ecd81c66cc
Lee_D <leegreat21@gmail.com>
2024-01-18 16:56:40 +0200

    EditProfileCondition

--- PartyMaker/src/pages/userPage/UserPage.tsx
+++ PartyMaker/src/pages/userPage/UserPage.tsx
@@ -10,25 +10,23 @@ import EditProfileModal from "../../components/editProfile/EditProfile";
 import { userSelector } from "../../features/loggedInUser/userSlice";
 import { editUserApi } from "../../features/loggedInUser/userAPI";
 import ResetPassword from "../../components/rest-password/ResetPassword";
-import {updatePasswordApi} from "../../features/loggedInUser/userAPI";
-
+import { updatePasswordApi } from "../../features/loggedInUser/userAPI";
 
 const UserPage: React.FC = () => {
   const user: User | null = useAppSelector(userSelector);
   const dispatch = useAppDispatch();
-  
+
   const [showEditProfile, setShowEditProfile] = useState(false);
- 
 
   const handleSaveProfile = async (editedUser: User) => {
     try {
-     const respons = await dispatch(editUserApi(editedUser))
-     
-     if (respons) {
-       alert("Profile updated successfully");
-       //navigate("/");
+      const respons = await dispatch(editUserApi(editedUser));
+
+      if (respons) {
+        alert("Profile updated successfully");
+        //navigate("/");
       }
-   
+
       setShowEditProfile(false);
     } catch (error) {
       console.error(error);
@@ -47,79 +45,74 @@ const UserPage: React.FC = () => {
   const handleResetPassword = () => {
     setShowResetPassword(true);
   };
-  
+
   const handleCloseResetPassword = () => {
     setShowResetPassword(false);
   };
-  const handleSaveResetPassword = async (user_id: number,password: string, newPassword: string, role: string) => {
+  const handleSaveResetPassword = async (
+    user_id: number,
+    password: string,
+    newPassword: string,
+    role: string
+  ) => {
     try {
-      debugger
-      const args = {user_id, password, newPassword, role };
+      debugger;
+      const args = { user_id, password, newPassword, role };
       const response = await dispatch(updatePasswordApi(args));
-  
+
       if (response) {
         alert("Password updated successfully");
         // navigate("/");
       }
-  
+
       setShowResetPassword(false);
     } catch (error) {
       console.error(error);
     }
   };
-  
-
 
   return (
- <div className="user-page-main">
+    <div className="UPmain">
     <NavBar />
-  <div className="user-page">
-    
-
-    <div className="user-page__details">
-    {user ? (
-          <>
-            <h1>Personal Information</h1>
-            <h2>{user.username}</h2>
-            <h2>{user.email}</h2>
-            <h2>{user.first_name}</h2>
-            <h2>{user.last_name}</h2>
-            <h2>{user.address}</h2>
-            <h2>{user.phone_number}</h2>
-            
-          </>
-        ) : (
-          <>
-           <p>no user found</p>
-          </>
-        )}
-    
-        
-
-    </div>
-    <div className="user-page__prties"></div>
-
-    </div>
-
-
-    <button className="editProfileBtn" onClick={handleEditProfile}>
-        Edit Profile
-      </button>
-      {showEditProfile && (
-        // Use the non-null assertion operator (!) here
-        <EditProfileModal user={user!} onSave={handleSaveProfile} onClose={handleCloseEditProfile} />
+    <div className="userPage">
+      {showEditProfile ? (
+        // If showEditProfile is true, render the edit profile form
+        <>
+          <EditProfileModal user={user!} onSave={handleSaveProfile} onClose={handleCloseEditProfile} />
+        </>
+      ) : (
+        // If showEditProfile is false, render the user details
+        <>
+          <div className="UPdetails">
+            {user ? (
+              <>
+                <h1>Personal Information</h1>
+                <h2>{user.username}</h2>
+                <h2>{user.email}</h2>
+                <h2>{user.first_name}</h2>
+                <h2>{user.last_name}</h2>
+                <h2>{user.address}</h2>
+                <h2>{user.phone_number}</h2>
+              </>
+            ) : (
+              <>
+                <p>no user found</p>
+              </>
+            )}
+          </div>
+          <div className="user-page__prties"></div>
+        </>
       )}
 
-<button className="resetPasswordBtn" onClick={handleResetPassword}>
-  Reset Password
-</button>
-
-{showResetPassword && (
-  <ResetPassword user={user!} onClose={handleCloseResetPassword} onSave={handleSaveResetPassword} />
-)}
+      <button className="editProfileBtn" onClick={handleEditProfile}>
+        Edit Profile
+      </button>
+      {showResetPassword && (
+        <ResetPassword user={user!} onClose={handleCloseResetPassword} onSave={handleSaveResetPassword} />
+      )}
       <h2>My Events</h2>
     </div>
-  );
-};
-
+  </div>
+);
+      }
 export default UserPage;
