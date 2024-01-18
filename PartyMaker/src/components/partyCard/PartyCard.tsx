4b632372f4f7040aa3e5e3ddd34556e991287965
Lee_D <leegreat21@gmail.com>
2024-01-18 23:17:52 +0200

    date-correction

--- PartyMaker/src/components/partyCard/PartyCard.tsx
+++ PartyMaker/src/components/partyCard/PartyCard.tsx
@@ -1,8 +1,7 @@
 // PartyCard.jsx
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Party } from '../../types-env';
 import './partyCard.scss';
-import axios from 'axios';
 
 type PartyCardProps = {
   party: Party;
@@ -13,6 +12,11 @@ const PartyCard: React.FC<PartyCardProps> = ({ party }) => {
     console.log(party);
   }, [party.party_id]);
 
+  const formatDate = (dateString: string) => {
+    const options: Intl.DateTimeFormatOptions = { day: '2-digit', month: '2-digit', year: 'numeric' };
+    return new Date(dateString).toLocaleDateString('en-US', options).replace(/(\d+)\/(\d+)\/(\d+)/, '$2/$1/$3');
+  };
+
   return (
     <div className="partyCard">
       {party ? (
@@ -20,8 +24,10 @@ const PartyCard: React.FC<PartyCardProps> = ({ party }) => {
           <h2 className='partyName'>{party.party_name}</h2>
           <div className="partyContent">
             <div className="imageOverlay" style={{ backgroundImage: `url(data:image/png;base64,${party.party_img_data})` }}>
-              {party.party_date !== null ? (<div className='partyDetails'>
-                <p>{party.category_description} | {party.party_location} | {new Date(party.party_date).toLocaleDateString()}</p></div>
+              {party.party_date !== null ? (
+                <div className='partyDetails'>
+                  <p>{party.category_description} | {party.party_location} | {formatDate(party.party_date)}</p>
+                </div>
               ) : (
                 <p>Date: Not available</p>
               )}
