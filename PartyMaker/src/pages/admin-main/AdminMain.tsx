f54d624e4280da81de66597acde35f1ccf8500bd
Lee_D <leegreat21@gmail.com>
2024-01-21 17:16:54 +0200

    general-reusable-btn

--- PartyMaker/src/pages/admin-main/AdminMain.tsx
+++ PartyMaker/src/pages/admin-main/AdminMain.tsx
@@ -1,22 +1,23 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
-import { useAppDispatch } from '../../app/hook';
-import NavBar from '../../components/navBar/NavBar';
-import './AdminMain.scss' 
-
+import { useNavigate } from "react-router-dom";
+import { useAppDispatch } from "../../app/hook";
+import NavBar from "../../components/navBar/NavBar";
+import "./AdminMain.scss";
+import GeneralBtn from "../../components/generalBtn/GeneralBtn";
 
 const AdminMain = () => {
-        const navigate = useNavigate();
-        const dispatch = useAppDispatch();
-    return (
-        <div className='adminMain'>
-          <NavBar />
-        <h1>Admin Main</h1>
-        <button onClick={()=>navigate("/admin/parties")}>Parties</button>
-        <button onClick={()=>navigate("/admin/users")}>Users</button>
-      
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  return (
+    <div className="adminMain">
+      <NavBar />
+      <h1>Admin Main</h1>
+      <GeneralBtn
+        buttonText="Parties"
+        onClick={() => navigate("/admin/parties")}
+      />
+      <GeneralBtn buttonText="Users" onClick={() => navigate("/admin/users")} />
     </div>
-  )
-}
+  );
+};
 
-export default AdminMain
+export default AdminMain;
