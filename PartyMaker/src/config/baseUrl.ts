52479c7755caeeff8de37bff20a7d6e910eac1c1
gili <gilimena95@gmail.com>
2024-01-24 21:46:58 +0200

    WIP

--- PartyMaker/src/config/baseUrl.ts
+++ PartyMaker/src/config/baseUrl.ts
@@ -1,1 +1,1 @@
-export const base_url = "http://localhost:8000"
+export const base_url = "https://partymaker-backend.onrender.com"
--- server/config/allowedOrigins.ts
+++ server/config/allowedOrigins.ts
@@ -1,4 +1,4 @@
 
 // const url = process.env.CLIENT_PROD_URL
-export const allowedOrigins = ["https://gili-example-client-deploy.onrender.com"];
+export const allowedOrigins = ["https://party-maker.onrender.com"];
 
